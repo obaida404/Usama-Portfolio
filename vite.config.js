d3ee0709943e14c2138a3f29a312c7aad9c4a380
Obaida <obaidamobdy2@gmail.com>
2025-08-21 03:19:41 +0800

    fix: explicitly set root directory for vite

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path' // <-- Import the path module
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/Usama-Portfolio/',
-  root: '.', // <-- ADD THIS LINE
+  root: path.resolve(__dirname), // <-- Use absolute path to current directory
 })
